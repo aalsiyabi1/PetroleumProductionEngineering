5de0ab89030e890dd80656628669168a0110d2eb
aalsiyabi1 <007alsiyabi@gmail.com>
2021-09-04 12:41:23 +0400

    Mid-depth Pressure fsolve

--- AvgTZ.py
+++ AvgTZ.py
@@ -3,6 +3,7 @@ import math
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy import optimize
+import numpy as np
 
 # %%
 gSG = 0.71
@@ -39,41 +40,39 @@ def zFactor(p, t, gSG):
 # %%
 f = (1 / (1.74 - 2 * math.log10(2 * rel_roughness))) ** 2
 
-#%%
+# %%
 depth = [x for x in range(0, L + 1, int(L / 2))]
 T_hf_R = Thf + ((Twf - Thf) / L) * depth[0] + 460
 T_mf_R = Thf + ((Twf - Thf) / L) * depth[1] + 460
 T_wf_R = Thf + ((Twf - Thf) / L) * depth[-1] + 460
-Z_hf = zFactor(phf,T_hf_R,gSG)
-pzt_hf = phf/(Z_hf*T_hf_R)
-I_hf = pzt_hf / (0.001 *math.cos(math.radians(theta)) * (pzt_hf**2) + (0.6666*f*(qsc**2))/(d**5))
+Z_hf = zFactor(phf, T_hf_R, gSG)
+pzt_hf = phf / (Z_hf * T_hf_R)
+I_hf = pzt_hf / (0.001 * math.cos(math.radians(theta)) * (pzt_hf ** 2) + (0.6666 * f * (qsc ** 2)) / (d ** 5))
 
 # %%
 
+def pCalc(piz_guess):
+    p = piz_guess[0]
+    I = piz_guess[1]
+    z = piz_guess[2]
+
+    ppc = 677 + 15 * gSG - 37.5 * gSG ** 2
+    tpc = 168 + 325 * gSG - 12.5 * gSG ** 2
+
+    tpr = T_mf_R / tpc
+
+    f0 = z - ((1.39 * (tpr - 0.92) ** 0.5 - 0.36 * tpr - 0.101) + ((1 - (1.39 * (tpr - 0.92) ** 0.5 - 0.36 * tpr - 0.101)) /
+                                                                  (math.exp((0.62 - 0.23 * tpr) * (p / ppc) + ((0.066 / (tpr - 0.86)) - 0.037) * ((p / ppc) ** 2) + ((0.32 * ((p / ppc) ** 6)) / (10 ** (9 * (tpr - 1))))))
+                                                                  + (0.132 - 0.32 * math.log10(tpr)) * ((p / ppc) ** (10 ** (0.3106 - 0.49 * tpr + 0.1824 * (tpr ** 2))))))
+    f1 = I - ((p / (z * T_mf_R)) / (0.001 * math.cos(math.radians(theta)) * ((p / (z * T_mf_R)) ** 2) + (0.6666 * f * (qsc ** 2)) / (d ** 5)))
+    f2 = p - phf - (18.75*gSG*L/(I_hf+I))
+    return np.array([f0,f1,f2])
 
 
-#
-# p_guess = phf+50
-# z_guess = zFactor(p_guess,T_mf_R,gSG)
-#
-# obj_fun = lambda Y: (Y - phf - (18.75*gSG*L/(I_hf+((Y/(z_guess*T_mf_R)) /
-#                                                    (0.001 *math.cos(math.radians(theta)) * ((Y/(z_guess*T_mf_R))**2) +
-#                                                     (0.6666*f*(qsc**2))/(d**5))))))
-# Y_soln = optimize.fsolve(obj_fun, 0)
-# fY_soln = obj_fun(Y_soln)
-# # print(Y_soln,fY_soln)
-#
-# pmf = Y_soln
-#
+piz_guess0 = np.array([phf, I_hf, Z_hf])
+piz = optimize.fsolve(pCalc,piz_guess0)
+pmf = piz[0]
+print(piz,pCalc(piz))
+print(zFactor(pmf,T_mf_R,gSG))
 
-
-
-
-#%%
-'''
-fY = lambda Y: (P[1] - P[0] - (18.75*gSG*L/(I[1]+I[0]))
-Y_soln = optimize.fsolve(fY, 0)
-fY_soln = fY(Y_soln)
-print(Y_soln,fY_soln)
-'''
-
+# %%
